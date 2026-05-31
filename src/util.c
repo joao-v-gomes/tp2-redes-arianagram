0624078fb0dff3631a9de665bc82446f7af45357
Joao Gomes <joao.victor214@gmail.com>
2026-05-31 16:45:09 -0300

    using htonl and ntohl in all msgs

--- src/util.c
+++ src/util.c
@@ -4,23 +4,14 @@
 // convertendo os campos da mensagem de host para network byte order antes de enviar,
 // e de network para host byte order após receber.
 void messageHostToNetwork(Message *msg) {
-    // msg->type = htonl(msg->type);
-    // for (int i = 0; i < 5; i++) {
-    //     msg->guess[i] = htonl(msg->guess[i]);
-    //     msg->feedback[i] = htonl(msg->feedback[i]);
-    // }
-    // msg->attempts = htonl(msg->attempts);
-    // msg->win_status = htonl(msg->win_status);
+    msg->type   = htons(msg->type);
+    msg->msg_id = htonl(msg->msg_id);
+    // username e content são char[], sem conversão de byte order necessária.
 }
 
 void messageNetworkToHost(Message *msg) {
-    // msg->type = ntohl(msg->type);
-    // for (int i = 0; i < 5; i++) {
-    //     msg->guess[i] = ntohl(msg->guess[i]);
-    //     msg->feedback[i] = ntohl(msg->feedback[i]);
-    // }
-    // msg->attempts = ntohl(msg->attempts);
-    // msg->win_status = ntohl(msg->win_status);
+    msg->type   = ntohs(msg->type);
+    msg->msg_id = ntohl(msg->msg_id);
 }
 
 void printMsg(Message *msg) {
